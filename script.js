6b6d4ed8fd9ec6a2b5f4e57a9524e8bf2b22a78c
Lasse Holm Gundesen <57224603+la8231@users.noreply.github.com>
2020-03-10 13:41:55 +0100

    Reedit of stuff

---
+++ script.js
@@ -0,0 +1,39 @@
+var stt = {
+    op,
+    ned
+}
+
+var stm = {
+    op,
+    ned
+}
+
+var slt = {
+    op,
+    ned
+}
+var slm = {
+    op,
+    ned
+}
+
+
+if(stt.op -----) {
+    ++1
+} else {
+    return
+}
+
+
+
+if(StartT == Gh && StartM == Gm) {
+    lights = true
+} else {
+    return}
+
+if(lights == true){
+    console.log('Lights are on')
+} else {
+    console.log('Lights are off')
+}
+
